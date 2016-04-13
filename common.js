c013b8bb68979433bfa1baa6cf563b8ec2f7c4db
jr conlin <jrconlin+git@gmail.com>
2016-04-13 16:33:13 -0700

    feat: Chrome rework
    Test the page against Chrome Canaray (52.0.2708.0)
    
    * Moved common functions to separate file.
    * reduced some call complexity
    * attempt to deal with chrome not handling "raw" keys.
    * Sadly, Chrome's ECDSA and ECDH are not yet up to snuff. Added
    a preflight check for the page to try and anticipate major errors.

---
+++ common.js
@@ -0,0 +1,88 @@
+var mzcc = {
+    ord: function(c){
+        /* return an ordinal for a character
+        */
+        return c.charCodeAt(0);
+    },
+
+    chr: function(c){
+        /* return a character for a given ordinal
+        */
+        return String.fromCharCode(c);
+    },
+
+    toUrlBase64: function(data) {
+        /* Convert a binary array into a URL safe base64 string
+        */
+        return btoa(data)
+            .replace(/\+/g, "-")
+            .replace(/\//g, "_");
+    },
+
+    fromUrlBase64: function(data) {
+        /* return a binary array from a URL safe base64 string
+        */
+        return atob(data
+            .replace(/\-/g, "+")
+            .replace(/\_/g, "/"));
+    },
+
+    _strToArray: function(str) {
+        /* convert a string into a ByteArray
+         *
+         * TextEncoders would be faster, but have a habit of altering
+         * byte order
+         */
+        let split = str.split("");
+        let reply = new Uint8Array(split.length);
+        for (let i in split) {
+            reply[i] = this.ord(split[i]);
+        }
+        return reply;
+    },
+
+    _arrayToStr: function(array) {
+        /* convert a ByteArray into a string
+         */
+        return String.fromCharCode.apply(null, new Uint8Array(array));
+    },
+
+    rawToJWK: function(raw, ops) {
+    /* convert a URL safe base64 raw key to jwk format
+    */
+        if (typeof(raw) == "string") {
+            raw = this._strToArray(this.fromUrlBase64(raw));
+        }
+        // Raw is supposed to start with a 0x04, but some libraries don't. sigh.
+        if (raw.length == 65 && raw[0] != 4) {
+            throw new Error('ERR_PUB_KEY');
+        }
+
+        raw= raw.slice(-64);
+        let x = this.toUrlBase64(String.fromCharCode.apply(null,
+            raw.slice(0,32)));
+        let y = this.toUrlBase64(String.fromCharCode.apply(null,
+            raw.slice(32,64)));
+
+        // Convert to a JWK and import it.
+        let jwk = {
+            crv: "P-256",
+            ext: true,
+            key_ops: ops,
+            kty: "EC",
+            x: x,
+            y, y
+        };
+
+        return jwk
+    },
+
+    JWKToRaw: function(jwk) {
+        /* Convert a JWK object to a "raw" URL Safe base64 string
+        */
+        xv = this.fromUrlBase64(jwk.x);
+        yv = this.fromUrlBase64(jwk.y);
+        return this.toUrlBase64("\x04" + xv + yv);
+    },
+}
+
